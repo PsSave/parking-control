ecfcb6aa0e613f389ed6e7c67e10143c8e037688
Pedro Samuel <pedrosviki1265@gmail.com>
2023-04-06 14:55:23 -0400

    Implemented POST method

--- src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -1,15 +1,14 @@
 package com.api.parkingcontrol.dtos;
 
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public class ParkingSpotDto {
 
-  @NotBlank //verifica se o campo não é nulo
+    @NotBlank
     private String parkingSpotNumber;
     @NotBlank
-    @Size(max = 7)//limita numeros de caracteres
+    @Size(max = 7)
     private String licensePlateCar;
     @NotBlank
     private String brandCar;
@@ -25,51 +24,66 @@ public class ParkingSpotDto {
     private String block;
 
     public String getParkingSpotNumber() {
-      return parkingSpotNumber;
+        return parkingSpotNumber;
     }
+
     public void setParkingSpotNumber(String parkingSpotNumber) {
-      this.parkingSpotNumber = parkingSpotNumber;
+        this.parkingSpotNumber = parkingSpotNumber;
     }
+
     public String getLicensePlateCar() {
-      return licensePlateCar;
+        return licensePlateCar;
     }
+
     public void setLicensePlateCar(String licensePlateCar) {
-      this.licensePlateCar = licensePlateCar;
+        this.licensePlateCar = licensePlateCar;
     }
+
     public String getBrandCar() {
-      return brandCar;
+        return brandCar;
     }
+
     public void setBrandCar(String brandCar) {
-      this.brandCar = brandCar;
+        this.brandCar = brandCar;
     }
+
     public String getModelCar() {
-      return modelCar;
+        return modelCar;
     }
+
     public void setModelCar(String modelCar) {
-      this.modelCar = modelCar;
+        this.modelCar = modelCar;
     }
+
     public String getColorCar() {
-      return colorCar;
+        return colorCar;
     }
+
     public void setColorCar(String colorCar) {
-      this.colorCar = colorCar;
+        this.colorCar = colorCar;
     }
+
     public String getResponsibleName() {
-      return responsibleName;
+        return responsibleName;
     }
+
     public void setResponsibleName(String responsibleName) {
-      this.responsibleName = responsibleName;
+        this.responsibleName = responsibleName;
     }
+
     public String getApartment() {
-      return apartment;
+        return apartment;
     }
+
     public void setApartment(String apartment) {
-      this.apartment = apartment;
+        this.apartment = apartment;
     }
+
     public String getBlock() {
-      return block;
+        return block;
     }
+
     public void setBlock(String block) {
-      this.block = block;
+        this.block = block;
     }
 }
--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -3,7 +3,10 @@ package com.api.parkingcontrol.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ParkingSpotService {
@@ -13,5 +16,22 @@ public class ParkingSpotService {
   @Autowired //cria ponto de injeção de dependencia 
   public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
     this.parkingSpotRepository = parkingSpotRepository;
+  }
+
+  @Transactional
+  public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+    return parkingSpotRepository.save(parkingSpotModel);
+  }
+
+  public boolean existsByLicensePlateCar(String licensePlateCar) {
+    return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+  }
+
+  public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+    return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+  }
+
+  public boolean existsByApartmentAndBlock(String apartment, String block) {
+    return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
   }
 }
